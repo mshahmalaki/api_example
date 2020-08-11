6a7c8bd01a99415583313e23c1697aef69abc43c
mshahmalaki <mshahmalaki@gmail.com>
2020-08-11 23:23:01 +0430

    Run Program with flask run

--- api.py
+++ api.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisissecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
